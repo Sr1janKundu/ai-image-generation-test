248024e5e248870c106687127844dd615c51cd4d
Srijan Kundu <srijan.kundu7@gmail.com>
2024-11-20 19:45:46 +0530

    Nov 20, 2024

--- dc-gan/model.py
+++ dc-gan/model.py
@@ -5,6 +5,7 @@ Implementation of DCGAN paper, (https://arxiv.org/abs/1511.06434)
 import torch
 import torch.nn as nn
 
+
 class Discriminator(nn.Module):
     def __init__(self, channels_img, features_d):
         """
@@ -83,7 +84,7 @@ class Generator(nn.Module):
             features_g ():
         """
         super(Generator, self).__init__()
-        self.net = nn.Sequential(
+        self.gen = nn.Sequential(
             # Input: N x z_dim x 1 x 1
             self._block(z_dim,
                         features_g * 16,
@@ -93,19 +94,19 @@ class Generator(nn.Module):
             self._block(features_g * 16,
                         features_g * 8,
                         kernel_size=4,
-                        stride=1,
-                        padding=0),     # 8x8
+                        stride=2,
+                        padding=1),     # 8x8
             self._block(features_g * 8,
                         features_g * 4,
                         kernel_size=4,
-                        stride=1,
-                        padding=0),      # 16x16
+                        stride=2,
+                        padding=1),      # 16x16
             self._block(features_g * 4,
                         features_g * 2,
                         kernel_size=4,
-                        stride=1,
-                        padding=0),     # 32x32
-            nn.ConvTranspose2d(features_g * 4,
+                        stride=2,
+                        padding=1),     # 32x32
+            nn.ConvTranspose2d(features_g * 2,
                                channels_img,
                                kernel_size=4,
                                stride=2,
@@ -139,6 +140,10 @@ class Generator(nn.Module):
             nn.ReLU(),
         )
 
+    def forward(self, x):
+        return self.gen(x)
+
+
 def init_weights(model):
     """
     Model weight initialization according to paper
@@ -151,3 +156,20 @@ def init_weights(model):
     for m in model.modules():
         if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.BatchNorm2d)):
             nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+
+def test():
+    N, in_channels, H, W = 8, 3, 64, 64
+    z_dim = 100
+    x = torch.randn((N, in_channels, H, W))
+    disc = Discriminator(in_channels, 8)
+    init_weights(disc)
+    assert disc(x).shape == (N, 1, 1, 1), "Discriminator test failed"
+    gen = Generator(z_dim, in_channels, 8)
+    init_weights(gen)
+    z = torch.randn((N, z_dim, 1, 1))
+    assert gen(z).shape == (N, in_channels, H, W), "Generator test failed"
+    print("Success, tests passed!")
+
+if __name__ == '__main__':
+    test()

f43196acb3dc6fecc7a3a3da7201c36c93a622f5
Srijan Kundu <srijan.kundu7@gmail.com>
2024-12-16 20:00:20 +0530

    Dec 16, 2024

--- stack-gan/config.py
+++ stack-gan/config.py
@@ -6,7 +6,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 image_dir = '/home/srijan/Desktop/Srijan-files/seq2seq-demo/image_captioning/Flickr_8k_Images_Captions/flickr8k/images/'
 all_captions_file = '/home/srijan/Desktop/Srijan-files/seq2seq-demo/image_captioning/Flickr_8k_Images_Captions/flickr8k/captions.txt'
 
-load_stage1 = True
+load_stage1 = False
 load_stage2 = False
 
 hyperparameters = {
@@ -19,8 +19,8 @@ hyperparameters = {
     "gen_loss_kld_reg_param": 1,    # lambda
     "train_stage1_for": 25,         # number of epochs to train stage 1 gan first (do 600 to follow paper)
     "start_lr": 2e-4,
-    "batch_size_stage1": 32,
-    "batch_size_stage2": 16,
+    "batch_size_stage1": 64,
+    "batch_size_stage2": 64,
 
 }
 
